ef16a148e918d28d545bb4c1ff91f8f5ec966310
Sam Holdsworth <sdholdsworth86@hotmail.com>
2020-08-15 12:07:32 +0100

    updates and improvements made

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,7 +7,6 @@ const Footer = () => {
             <footer className="footer">
                 <span>Copyright&nbsp;<i id="copy" className="fa fa-copyright"></i>&nbsp;<strong className="red">bangingmickdj.co.uk</strong>&nbsp;All Rights Reserved</span>
                 <div id="social">
-                    {/*<span>Follow Me On:</span>*/}
                     <a href="https://www.facebook.com/DJ-Banging-mick-101634288151123/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-square"></i></a>
                     <a href="https://twitter.com/bangingmick" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter-square"></i></a>
                     <a href="https://uk.linkedin.com/in/mick-lewis-06a545106" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-square"></i></a>
