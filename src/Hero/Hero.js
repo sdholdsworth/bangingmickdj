5bf256def405b6ade968dca6a2013a92723a517e
Sam Holdsworth <sdholdsworth86@hotmail.com>
2020-08-15 21:55:25 +0100

    minor edits made

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -15,16 +15,16 @@ const Hero = () => {
     return (
         <div className="hero-container">
 			<figure>	
-				<img src={ Slider1 } alt="pic 1" />
-				<img src={ Slider2 } alt="pic 2" />
-				<img src={ Slider3 } alt="pic 3" />
-				<img src={ Slider4 } alt="pic 4" />
-				<img src={ Slider5 } alt="pic 5" />
-				<img src={ Slider6 } alt="pic 6" />
-				<img src={ Slider7 } alt="pic 7" />
-				<img src={ Slider8 } alt="pic 8" />
-				<img src={ Slider9 } alt="pic 9" />
-				<img src={ Slider1 } alt="pic 1" />
+				<img src={ Slider1 } alt="slider 1" />
+				<img src={ Slider2 } alt="slider 2" />
+				<img src={ Slider3 } alt="slider 3" />
+				<img src={ Slider4 } alt="slider 4" />
+				<img src={ Slider5 } alt="slider 5" />
+				<img src={ Slider6 } alt="slider 6" />
+				<img src={ Slider7 } alt="slider 7" />
+				<img src={ Slider8 } alt="slider 8" />
+				<img src={ Slider9 } alt="slider 9" />
+				<img src={ Slider1 } alt="slider 1" />
 			</figure>
 			<div className="hero-overlay">
 				<h1 className="white"> <span className="red">DJ&nbsp;</span>Banging Mick</h1>
