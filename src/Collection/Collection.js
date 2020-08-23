ffa745f7c108d76f40f4dc5fa19b61feaf475788
Sam Holdsworth <sdholdsworth86@hotmail.com>
2020-08-23 22:22:26 +0100

    major changes made

--- src/Collection/Collection.js
+++ src/Collection/Collection.js
@@ -5,7 +5,45 @@ const Collection = () => {
     return (
         <div>
             <section id="collection" className="section-light">
-                <h2>My Collection</h2>
+                <p>Here's a little sample of some of my collection.....</p>
+                <div id="collection-samples">                    
+                    <iframe 
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Progressive Mayhem U G Media XL"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/876828184&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">                       
+                    </iframe>
+                    <iframe 
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="The Banging Mick Legacy Vol 6"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/864797386&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                    </iframe>
+                    <iframe 
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="The Rising"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/798745843&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                    </iframe>
+                    <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="UG Media Show 8"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/791343832&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                    </iframe>
+                    <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Tranzfuzion Reload"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/698782063&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                    </iframe>
+                </div>
+                <p>Like what you see? ... view more of my collection <a href="https://soundcloud.com/bangingmickdj" target="_blank" rel="noopener noreferrer">here</a></p>
             </section>
         </div>
     )
