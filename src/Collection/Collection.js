51389636c1d7186a50f6e7a39169595d90a800e2
Sam Holdsworth <sdholdsworth86@hotmail.com>
2020-08-24 21:50:11 +0100

    final enhancements made

--- src/Collection/Collection.js
+++ src/Collection/Collection.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Collection.scss';
+import DJCollection from '../img/dj_collection.png';
 
 const Collection = () => {
     return (
         <div>
             <section id="collection" className="section-light">
-                <p>Here's a little sample of some of my collection.....</p>
+                <div>
+                    <p>Here's a little taster of some of my collection for you... <strong><i>ENJOY!</i></strong></p>
+                </div>
                 <div id="collection-samples">                    
                     <iframe 
                         scrolling="no" 
@@ -42,8 +45,116 @@ const Collection = () => {
                         title="Tranzfuzion Reload"
                         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/698782063&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
                     </iframe>
+                    <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Funky House"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/763970311&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                    </iframe>
+                    <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="3rd Show UG Media"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/750480313&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Old Skool Set on UG Media"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/753449665&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Epic Vocal Trance"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/726782242&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Techno Mix"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/875266525&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Love Of Trance"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/783756436&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="First Show UG Media Epic Trance"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/741959530&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Live on UGC Lanzarote Part 4"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/717413485&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Lockdown Legends Banging Mick on UG Media XL Trance Essentials"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/831258703&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="My Chistmas Day Set Mark Sherry"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/733866676&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Old Skool Mini Mix"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/693921559&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Sand Storm"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/690524185&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Typhoon"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/688138777&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Time Warp"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/618717969&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
+                     <iframe
+                        scrolling="no" 
+                        frameborder="no" 
+                        allow="autoplay" 
+                        title="Walking On Electric Dreams part 1"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/605959380&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                     </iframe>
                 </div>
-                <p>Like what you see? ... view more of my collection <a href="https://soundcloud.com/bangingmickdj" target="_blank" rel="noopener noreferrer">here</a></p>
+                <div>
+                    <p>...Like what you see?... view more of my collection <a href="https://soundcloud.com/bangingmickdj" target="_blank" rel="noopener noreferrer">here</a></p>
+                    <img src={ DJCollection } alt="dj collection"></img>
+                </div>
             </section>
         </div>
     )
