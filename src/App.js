aa56c8fa19b4540c2f08b41408818dca363fe1d1
Sam Holdsworth <sdholdsworth86@hotmail.com>
2020-09-07 18:49:15 +0100

    added smooth scroll polyfill to improve cross-browser compatibility

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'normalize.css/normalize.css';
 import './styles/main.scss';
+import smoothscroll from 'smoothscroll-polyfill';
 import Header from './Header/Header';
 import Hero from './Hero/Hero';
 import About from './About/About';
@@ -8,6 +9,9 @@ import Gallery from './Gallery/Gallery';
 import Collection from './Collection/Collection';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
+
+//initiate polyfill for smooth scroll (cross-browser smooth scroll behaviour fix)
+smoothscroll.polyfill();
 
 function App() {
   return (
