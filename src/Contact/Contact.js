1995d81c6bde358662d0ca792da9913e791fb645
sdholdsworth <sdholdsworth86@hotmail.com>
2020-11-20 14:07:43 +0000

    final essential contact form configuration & edits completed

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,41 +1,138 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Axios, db } from '../firebase/firebaseConfig';
 import './Contact.scss';
 import DJContact from '../img/dj_contact.png';
 import Email from '../img/email.png';
 
 const Contact = () => {
-    return (
-        <div>
-            <section id="contact" className="section-dark">
-                <div id="contact-msg">
-                    <p>If you're looking for a DJ for your event, look no further, DJ Banging Mick will offer you a professional, reputable and affordable service tailored to your needs to best suit your purpose.</p>
-                    <p>Please feel free to make a none obligatory enquiry by filling out this form and sending me your message and I will get back to you as soon as possible.</p>
-                    <p>I hope you have enjoyed viewing what I have to offer and hope to hear from you soon.</p>
-                </div>
-                <div id="contact-form">
-                    <form action="" method="get">
-                        <label>Your Name</label>
-                        <input type="text" name="name" id="name" required placeholder="what's your name?" />
-                        <label>Your Email</label>
-                        <input type="email" name="email" id="email" required placeholder="what's your email address?" />
-                        <label>Your Number</label>
-                        <input type="tel" name="number" id="number" required placeholder="what's your contact number?" />
-                        <label>Your Event</label>
-                        <textarea name="event" id="event" required placeholder="please let me know of any key details relating to your enquiry, for example: are you looking to hire a DJ for a: venue / party / function / festival etc... location, dates etc... " />
-                        <button type="submit">
-                            <img src={ Email } alt="email"/>
-                            <span>Send Message</span>
-                        </button>
-                        <p>Alternatively, feel free to contact me by phone on:</p>
-                        <a className="red" href="tel:+34609765797">+34 609 76 57 97</a>
-                    </form>
-                </div>
-                <div id="dj-contact">
-                    <img src={ DJContact } alt="dj contact"></img>
-                </div>
-            </section>
-        </div>
-    )
-}
+    const [formData, setFormData] = useState({});
+  
+    //update state
+    const updateInput = e => {
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value
+      })
+    }
 
-export default Contact;
+    //handle form submission
+    const handleSubmit = event => {
+      event.preventDefault();
+      sendEmail();
+      setFormData({
+        name: '',
+        email: '',
+        number: '',
+        message: ''
+      });
+    }
+
+    //send email
+    const sendEmail = () => {
+      //post form entry data
+      Axios.post(
+        'https://us-central1-dj-banging-mick.cloudfunctions.net/submit',
+        formData
+      )
+        .then(res => {
+          db.collection('emails').add({
+            name: formData.name,
+            email: formData.email,
+            number: formData.number,
+            message: formData.message,
+            time: new Date()
+          })
+        })
+        //alert user message send was successful
+        .then(() => { alert(`\nThank you, your message has been submitted. \n\nDJ Banging Mick will be in contact with you shortly! 👍`);})
+        //alert user if an error occurs
+        .catch((error) => { alert(`Oops, something went wrong 😔 \n\nThe following error occurred: \n\n${error.message} \n\nPlease try again later or alternatively feel free to contact me by phone on: +34 609 76 57 97 \n\nWe apologize for any inconvenience this may cause.`);})
+    }
+  
+    return (
+      <div>
+        <section id="contact" className="section-dark">
+            <div id="contact-msg">
+                <p>If you're looking for a DJ for your event, look no further, DJ Banging Mick will offer you a professional, reputable and affordable service tailored to your needs to best suit your purpose.</p>
+                <p>Please feel free to make a none obligatory enquiry by filling out this form and sending me your message and I will get back to you as soon as possible.</p>
+                <p>I hope you have enjoyed viewing what I have to offer and hope to hear from you soon!</p>
+                <h3>DJ Banging Mick</h3>
+            </div>
+            <div id="contact-form">
+                <form onSubmit={handleSubmit}>
+                    <label>Your Name&nbsp; 
+                        <span 
+                            role="img" 
+                            aria-label="alphanumeric characters icon" 
+                            style={{fontSize: 1.5 + 'rem'}}>🔤
+                        </span>
+                    </label>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="what's your name?"
+                        onChange={updateInput}
+                        value={formData.name || ''}
+                        required
+                    />
+                    <label>Your Email&nbsp;
+                        <span 
+                            role="img" 
+                            aria-label="email icon" 
+                            style={{fontSize: 1.75 + 'rem'}}>📧
+                        </span>
+                    </label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="what's your email address?"
+                        onChange={updateInput}
+                        value={formData.email || ''}
+                        required
+                    />
+                    <label>Your Number&nbsp;
+                        <span 
+                            role="img" 
+                            aria-label="phone icon" 
+                            style={{fontSize: 1.6 + 'rem'}}>📞
+                        </span>
+                    </label>
+                    <input
+                        type="tel"
+                        name="number"
+                        placeholder="what's your contact number?"
+                        onChange={updateInput}
+                        value={formData.number || ''}
+                    />
+                    <label>Your Message&nbsp;
+                        <span 
+                            role="img" 
+                            aria-label="message icon" 
+                            style={{fontSize: 1.6 + 'rem'}}>📝
+                        </span>
+                    </label>
+                    <textarea
+                        type="text"
+                        name="message"
+                        placeholder="please let me know of any key details relating to your enquiry, for example: are you looking to hire a DJ for a: venue / party / function / festival etc... the location, the dates etc..."
+                        onChange={updateInput}
+                        value={formData.message || ''}
+                        required
+                    ></textarea>
+                    <button type="submit">
+                        <img src={ Email } alt="send email"/>
+                        <span>Send Message</span>
+                    </button>
+                    <p>Alternatively, feel free to contact me by phone on:</p>
+                    <a className="red" href="tel:+34609765797">+34 609 76 57 97</a>
+                </form>
+            </div>
+            <div id="dj-contact">
+                <img src={ DJContact } alt="dj contact"></img>
+            </div>
+        </section>
+      </div>
+    )
+  }
+  
+  export default Contact;
